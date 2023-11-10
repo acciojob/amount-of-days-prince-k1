57e26933b55435780c87ede1fd80b4f8bd6a13c2
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-10 20:44:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+	let y = parseInt(year);
+	if(y%4 == 0){
+		if(y % 100 == 0 && y % 400 == 0){
+			return 366;
+		}
+		else if(y % 100 == 0 && y % 400 != 0){
+			return 365;
+		}
+		else{return 366}
+	}
+	else if(y % 4 != 0){alert(365)}
+	
+	
+}
+
+let year = prompt();
+alert(daysOfAYear(year))
